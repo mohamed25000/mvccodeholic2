7503139a9cd5103276139302fce331f3fc69552d
Mohamed Mennour <mohamedmennour25@gmail.com>
2022-12-14 11:29:52 +0100

    first commit

---
+++ core/Application.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\core;
+
+class Application
+{
+    public Router $router;
+    public Request $request;
+    public Response $response;
+    public static string $ROOT_DIR;
+    public static Application $app;
+    public Controller $controller;
+
+    /**
+     * @return Controller
+     */
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+
+    /**
+     * @param Controller $controller
+     */
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
+    }
+
+    public function __construct($rootPath)
+    {
+        self::$app = $this;
+        self::$ROOT_DIR = $rootPath;
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
+    }
+}
